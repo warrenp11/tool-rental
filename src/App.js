9aefea4bd499b75fd2886b0fe68943272bf5f538
Peter <warrenp11@gmail.com>
2022-02-01 16:06:58 -0500

    Contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Tools from "./components/Tools";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Tools />
+      <Contact />
     </div>
   );
 }
