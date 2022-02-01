da4f6e050bdb894456f1afde6c771d022b9e82f3
Peter <warrenp11@gmail.com>
2022-02-01 15:29:42 -0500

    Add About component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
