9f09275a202097a357452ca5445147c449abd18d
Peter <warrenp11@gmail.com>
2022-02-01 15:09:27 -0500

    Navbar and Home component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+          
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Tools
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+              
+            </ul>
+            <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">
+            Navbar
+          </a>
+            <button className="btn btn-outline-primary ms-auto px-4 rounded-pill">Login</button>
+            <button className="btn btn-outline-primary ms-3 px-4 rounded-pill">Register</button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
