107570b040e8d283cc49e9b95cb45364cc029c1b
Peter <warrenp11@gmail.com>
2022-02-01 16:15:58 -0500

    skeleton Footer component, not built

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Tools from "./components/Tools";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Tools />
       <Contact />
+      <Footer />
     </div>
   );
 }
