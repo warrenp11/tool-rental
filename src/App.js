cb8fb1678f0a4fcd965e7e2ac88b27329f76471c
Peter <warrenp11@gmail.com>
2022-02-01 15:53:14 -0500

    Tools component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Tools from "./components/Tools";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Tools />
     </div>
   );
 }
