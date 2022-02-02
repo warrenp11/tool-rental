026f897fc949341de37aa0e3da7997626a22aff9
Nsmall <nsmall1994@gmail.com>
2022-02-02 18:02:17 -0500

    app test

--- client/src/index.js
+++ client/src/index.js
@@ -14,14 +14,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-<<<<<<< HEAD
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
-=======
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 0dd1b187eb7609d25368d01a0f304066ca983e61
