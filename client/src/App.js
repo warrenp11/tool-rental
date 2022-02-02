90d4a87c0321668981a0b6afc7a5851e41c86671
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-02-01 22:54:40 -0500

    updated nav and json files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import React from 'react';
 import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,57 +1,119 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import Auth from "../../utils/auth";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-          
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/about">
-                  About
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/tools">
-                  Tools
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/contact">
-                  Contact
-                </a>
-              </li>
-              
-            </ul>
-            <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">
-            Tool Rentals
-          </a>
-            <NavLink to="/login" className="btn btn-outline-primary ms-auto px-4 rounded-pill">Login</NavLink>
-            <NavLink to="/signup" className="btn btn-outline-primary ms-3 px-4 rounded-pill">Sign Up</NavLink>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
-}
 
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+            <div class="container">
+
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a class="nav-link" href="/about">
+                      About
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a class="nav-link" href="/tools">
+                      Tools
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a class="nav-link" href="/contact">
+                      Contact
+                    </a>
+                  </li>
+
+                </ul>
+                <a class="navbar-brand fw-bolder fs-4 mx-auto" href="/tools">
+                  Tool Rentals
+                </a>
+                <NavLink to="/" onClick={() => Auth.logout()} className="btn btn-outline-primary ms-auto px-4 rounded-pill">Logout</NavLink>
+              </div>
+            </div>
+      );
+    } else {
+      return (
+            <div class="container">
+
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/about">
+                      About
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/tools">
+                      Tools
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/contact">
+                      Contact
+                    </a>
+                  </li>
+
+                </ul>
+                <a class="navbar-brand fw-bolder fs-4 mx-auto" href="/tools">
+                  Tool Rentals
+                </a>
+                <NavLink to="/login" className="btn btn-outline-primary ms-auto px-4 rounded-pill">Login</NavLink>
+                <NavLink to="/signup" className="btn btn-outline-primary ms-3 px-4 rounded-pill">Sign Up</NavLink>
+              </div>
+            </div>
+      )
+    }
+  }
+
+  return (
+    <header className="flex-row px-1">
+      {/* <h1>
+        <NavLink to="/">
+          <span role="img" aria-label="wrench">🔧</span>
+          Rent-a-Tool
+        </NavLink>
+      </h1> */}
+
+      <nav className="navbar navbar-expand-lg navbar-light">
+        {showNavigation()}
+      </nav>
+    </header>
+  )
+}
 export default Navbar;
