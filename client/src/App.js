965a861c911cbe33e6898e8c96a02dddb8f9bd2b
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2022-02-01 21:54:41 -0500

    npm installed, added utils folder and files, updated components and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -9,6 +12,25 @@ import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
